5e695d769f9e3e0ab463fb2659f9b85f8f86c845
joni moas <setpsistariasbaniou@gmail.com>
2020-12-03 23:38:53 +0200

    added primitive mouse support and image compression

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # goMirror
 
 Small app written in go, which streams the screen of the current system on 
-a web page in port 80. Just run, access the system's IP
+a web page in port 80.
+
+Mouse usage is also possible, click on the edges of the screen to move the mouse
+and on the center to perform a left click!
+
+Just run, access the system's IP
 address through a browser and click start!
 
 WebSocket       - https://github.com/gorilla/websocket
-ScreenShot      - https://github.com/kbinani/screenshot
-
+ScreenShot      - github.com/vova616/screenshot
+RobotGo         - github.com/go-vgo/robotgo
+Resize          - github.com/nfnt/resize
--- main.go
+++ main.go
@@ -5,31 +5,44 @@ import (
 	"encoding/base64"
 	"flag"
 	"html/template"
+	"image"
+	"image/color"
+	"image/draw"
 	"image/png"
 	"log"
 	"net/http"
 
+	"github.com/go-vgo/robotgo"
 	"github.com/gorilla/websocket"
-	"github.com/kbinani/screenshot"
+	"github.com/nfnt/resize"
+	"github.com/vova616/screenshot"
 )
 
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/screen", screen)
+	http.HandleFunc("/input", input)
 	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(":80", nil))
-
+	robotgo.MoveMouseSmooth(100, 200, 1.0, 100.0)
 }
 
 func makeImage() string {
-	bounds := screenshot.GetDisplayBounds(0)
-	img, err := screenshot.CaptureRect(bounds)
+	img, err := screenshot.CaptureScreen()
+	width := uint((float64(img.Bounds().Dx()) * 0.5))
+	height := uint((float64(img.Bounds().Dy()) * 0.5))
+	x, y := robotgo.GetMousePos()
+	c := color.White
+	pointer := image.Rect(x-5, y-5, x+5, y+5)
+	draw.Draw(img, pointer, &image.Uniform{c}, image.ZP, draw.Src)
+	img.Set(x, y, c)
+	resizedImg := resize.Resize(width, height, image.Image(img), resize.Lanczos3)
 	if err != nil {
 		panic(err)
 	}
 	var buff bytes.Buffer
-	png.Encode(&buff, img)
+	png.Encode(&buff, resizedImg)
 	encodedString := base64.StdEncoding.EncodeToString(buff.Bytes())
 	htmlImage := "data:image/png;base64," + encodedString
 	return htmlImage
@@ -37,13 +50,42 @@ func makeImage() string {
 
 var upgrader = websocket.Upgrader{}
 
-func echo(w http.ResponseWriter, r *http.Request) {
+func input(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Print("upgrade:", err)
 		return
 	}
-
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		_ = mt
+		log.Printf("recv: %s", message)
+		if string(string(message)[0]) == "M" {
+			switch string(string(message)[2]) {
+			case "U":
+				robotgo.MoveSmoothRelative(0, -10, 3.0, 30.0)
+			case "D":
+				robotgo.MoveSmoothRelative(0, 10, 3.0, 30.0)
+			case "L":
+				robotgo.MoveSmoothRelative(-10, 0, 3.0, 30.0)
+			case "R":
+				robotgo.MoveSmoothRelative(10, 0, 3.0, 30.0)
+			case "C":
+				robotgo.MouseClick("left", true)
+			}
+		}
+	}
+}
+func screen(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
@@ -51,19 +93,25 @@ func echo(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		log.Printf("recv: %s", message)
-		for {
-			err = c.WriteMessage(mt, []byte(makeImage()))
-			if err != nil {
-				log.Println("write:", err)
-				break
+		if string(message) == "go" {
+			for {
+				err = c.WriteMessage(mt, []byte(makeImage()))
+				if err != nil {
+					log.Println("write:", err)
+					break
+				}
 			}
 		}
-	}
 
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	homeTemplate.Execute(w, "ws://"+r.Host+"/echo")
+	sockets := map[string]interface{}{
+		"screen": "ws://" + r.Host + "/screen",
+		"input":  "ws://" + r.Host + "/input",
+	}
+	homeTemplate.Execute(w, sockets)
 }
 
 var homeTemplate, err = template.ParseGlob("*.tmpl")
